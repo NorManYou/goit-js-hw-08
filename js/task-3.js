609735306339e2343645bd72d7d65458a114be9f
Vitalii Shumakov <fafde8120@gmail.com>
2025-08-09 20:39:43 +0200

    full

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const searchInput = document.querySelector('#name-input');
-const spanId = document.querySelector('#name-output');
-
-searchInput.addEventListener('input', () => {
-  const trimmedValue = searchInput.value.trim();
-  spanId.textContent = trimmedValue ? trimmedValue : 'Anonymous';
-});
